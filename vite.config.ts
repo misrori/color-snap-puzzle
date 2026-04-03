339ee58132aa91d4abecfd3aa82b2d7ed68b534f
Mihály Orsós <ormraat.pte@gmail.com>
2026-04-03 22:21:40 +0200

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/color-snap-puzzle/",
+  base: process.env.VITE_BASE || "/color-snap-puzzle/",
   server: {
     host: "::",
     port: 8080,
